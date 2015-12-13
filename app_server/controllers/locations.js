4f1d967295f1de0ec54a7d0e82504890b4eaf703
Eldad Hagashi <eldadhagashi@Eldads-MacBook-Air.local>
2015-12-13 09:35:50 -0800

    Adding db configurations and model via mongoose

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,5 +1,43 @@
 module.exports.homeList = function(req, res, next){
-    res.render('locations-list', { title: 'Home' });
+    res.render('locations-list', { 
+        title: 'Seat-Tweet - Find the best tweets!',
+        pageHeader: {
+            title: 'Seat-Tweet',
+            strapLine: 'Find the best tweets!'
+        },
+        locations: [{
+            name: 'Albertsons Stadium',
+            address: 'Boise, ID',
+            rating: 4,
+            sports: 'CFB',
+            conference: 'MW',
+            isTop25: true,
+            geocode: "39.74388,-105.020097,0.25mi"
+        },{
+            name: 'LaVell Edwards Stadium',
+            address: 'Provo, UT',
+            rating: 4,
+            sports: 'CFB',
+            conference: 'Independent',
+            isTop25: true,
+            geocode: "39.74388,-105.020097,0.25mi" 
+        },{
+            name: 'Husky Stadium',
+            address: 'Seattle, WA',
+            rating: 5,
+            sports: 'CFB',
+            conference: 'Pac12',
+            isTop25: false,
+            geocode: "39.74388,-105.020097,0.25mi" 
+        }],
+        sports: [{
+            name: 'CFB',
+        },{
+            name: 'NFL',
+        },{
+            name: 'NBA',
+        }]
+    });
 };
 
 module.exports.locationStream = function(req, res, next){
