ccd046b42355b742e67f58100a361b9ae85cff18
Eldad Hagashi <eldadhagashi@Eldads-MacBook-Air.local>
2016-02-27 06:59:56 -0800

    removing extra forward slash from callback url

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ if(process.env.NODE_ENV ==='production'){
         consumer_secret: 'x8ZgwWOL2nObd78VmDptaHag2T8f09fUy5aB3rxzc4EqjlPhTk',
         access_token: '14613610-Td3X1f8zMAihYTjueJ8aS1t1LMatyx4fq30AbjLl0',
         access_token_secret: '6Snbh8yz9m7bvh5FHyLPmSUGnBpV08NiUpe6MpIsxvljr',
-        callbackURL: 'http://seat-tweet.herokuapp.com//api/login_cb'
+        callbackURL: 'http://seat-tweet.herokuapp.com/api/login_cb'
     };
 }
 else{
