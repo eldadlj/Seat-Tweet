b6b7c941d3cef8aae83d5412883555a223f2d56d
Eldad Hagashi <eldadhagashi@Eldads-MacBook-Air.local>
2016-02-28 14:41:30 -0800

    adding twttr widget code to controller

--- app_client/locationStreams/locationStreams.controller.js
+++ app_client/locationStreams/locationStreams.controller.js
@@ -4,6 +4,13 @@
         .controller('locationStreamsCtrl', locationStreamsCtrl);
     //this is required to get the twitter widget because of angular related twitter bug
 
+    window.twttr = (function (d,s,id) {
+        var t, js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return; js=d.createElement(s); js.id=id;
+        js.src="https://platform.twitter.com/widgets.js"; fjs.parentNode.insertBefore(js, fjs);
+        return window.twttr || (t = { _e: [], ready: function(f){ t._e.push(f) } });
+        }(document, "script", "twitter-wjs"));
+    
      function loadTweetsDelay(){
          setTimeout(function(){
              window.twttr.ready(function(twttr){
