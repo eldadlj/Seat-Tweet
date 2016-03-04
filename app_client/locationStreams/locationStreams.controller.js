67fad3872500975f4682b2dfbc90150b368c3485
Eldad Hagashi <eldadhagashi@Eldads-MacBook-Air.local>
2016-03-03 20:42:55 -0800

    moving widgests scripts to main page

--- app_client/locationStreams/locationStreams.controller.js
+++ app_client/locationStreams/locationStreams.controller.js
@@ -132,7 +132,7 @@
     
     
      function loadTweetsDelay(){
-         window.twttr.widgets.load();
+         //window.twttr.widgets.load();
         console.log('here')
          setTimeout(function(){
              window.twttr.ready(function(twttr){
