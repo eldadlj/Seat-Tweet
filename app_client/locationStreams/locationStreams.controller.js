a4d57bf7a221566ab39ac5c244f7bf96e4699038
Eldad Hagashi <eldadhagashi@Eldads-MacBook-Air.local>
2016-03-05 08:55:47 -0800

    testing

--- app_client/locationStreams/locationStreams.controller.js
+++ app_client/locationStreams/locationStreams.controller.js
@@ -1,11 +1,13 @@
 (function(){
     window.twttr = (function(d, s, id) {
+        console.log(d.getElementsByTagName(s));
   var js, fjs = d.getElementsByTagName(s)[0],
     t = window.twttr || {};
   if (d.getElementById(id)) return t;
   js = d.createElement(s);
   js.id = id;
   js.src = "https://platform.twitter.com/widgets.js";
+        console.log(js);
   fjs.parentNode.insertBefore(js, fjs);
  
   t._e = [];
